1194e9bb9afea378625bd5e85c2a7de7cb01675c
llChristopherll <141789855+llChristopherll@users.noreply.github.com>
2023-08-23 19:12:00 -0400

    Making Loops

---
+++ Web Fundamentals/HTML/Loops Algo/index.js
@@ -0,0 +1,15 @@
+for(var num = 1; num < 20; num+=2){
+    console.log(num)
+}
+
+let sum = 0;
+
+for (let i = 1; i <= 5; i++) {
+  sum += i;
+  console.log(`Number is: ${i}; Sum: ${sum}`);
+}
+
+
+
+
+
