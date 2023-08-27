0188c4d96903db944bd5c07c408c3ff98d3a45c5
llChristopherll <141789855+llChristopherll@users.noreply.github.com>
2023-08-27 13:16:49 -0400

    basic json

---
+++ Web Fundamentals/JavaScript/Objects/obj.js
@@ -0,0 +1,14 @@
+var dojo = {};                                 // creates an empty object
+dojo = {
+  name: 'Coding Dojo',                         // property can store a string
+  number_of_students: 25,                      // property can store a number
+  instructors: ['Andrew', 'Michael', 'Jay'],   // property can store arrays
+  location: {                                  // property can even store another object!
+    city: 'San Jose',
+    state: 'CA',
+    zipcode: 95112
+  }
+}                                              // access object properties with dot (.) notation
+console.log(dojo.name, dojo.number_of_students, dojo.instructors, dojo.location);
+console.log(dojo["name"]);                     // or bracket [] notation (note: specify key in quotes)
+
